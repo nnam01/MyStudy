9900f6451750b7d50163022a44db740924c161d8
nnam01 <isthenam@naver.com>
2025-07-27 21:43:55 +0900

    fix: 오타 수정

--- src/main/java/com/nnam01/MyStudy/user/dto/UserDto.java
+++ src/main/java/com/nnam01/MyStudy/user/dto/UserDto.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class UserDto {
-  private Long id;g
+  private Long id;
   private String username;
   private String email;
   private String imageUrl;
